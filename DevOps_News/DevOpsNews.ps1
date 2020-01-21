51fb2f3ccb20cb2c459756f4edda2dc689038abe
Michael Levan <mlevan1992@outlook.com>
2020-01-21 07:30:38 -0500

    Testing different ways of retrieving links to click

--- DevOps_News/DevOpsNews.ps1
+++ DevOps_News/DevOpsNews.ps1
@@ -1,12 +1,23 @@
 #Still in progress
 
 $microsoftDevOps = Invoke-WebRequest -Uri "https://www.google.com/search?hl=en&q=devops+in+microsoft+news"
-$out = $microsoftDevOps.AllElements.href | select -Skip 25 | Select -SkipLast 22
+$out = $microsoftDevOps.Links.href
+$out -replace "(http://.www.google.com*, http://.www.google.com/*, /search?hl*)",""
 
+
+Foreach($link in $out) {
+    $string = $link
+    $spl = $string -split "&amp"
+    [string]$url = $spl[0].Split("<a href=/url?q=") | Out-String
+    Start-Process $url
+}
+
+####################################################
 
 $string = '/url?q=https://devblogs.microsoft.com/devops/top-stories-from-the-microsoft-devops-community-2020-01-17/&amp;sa=U&amp;ved=2ahUKEwjizOWHsJPnAhUai1wKHcQ2AlUQFjAJegQIChAB&amp;usg=AOvVaw18yf0oz9CZ1-TtcfqVQr5F'
 $sep = '&amp'
 $out = $string -split "/&amp"
 
-$url = $out[0].Split("/url?q=")
-Start-Process $url
+[string]$url = $out[0].Split("/url?q=") | Out-String
+[string]$url2 = $url | Out-String
+Start-Process $url2
