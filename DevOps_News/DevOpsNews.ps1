e29b01881a5653cc030c40de715ac3787cdb5a58
Michael Levan <mlevan1992@outlook.com>
2020-01-20 19:36:57 -0500

    DevOps news

---
+++ DevOps_News/DevOpsNews.ps1
@@ -0,0 +1,12 @@
+#Still in progress
+
+$microsoftDevOps = Invoke-WebRequest -Uri "https://www.google.com/search?hl=en&q=devops+in+microsoft+news"
+$out = $microsoftDevOps.AllElements.href | select -Skip 25 | Select -SkipLast 22
+
+
+$string = '/url?q=https://devblogs.microsoft.com/devops/top-stories-from-the-microsoft-devops-community-2020-01-17/&amp;sa=U&amp;ved=2ahUKEwjizOWHsJPnAhUai1wKHcQ2AlUQFjAJegQIChAB&amp;usg=AOvVaw18yf0oz9CZ1-TtcfqVQr5F'
+$sep = '&amp'
+$out = $string -split "/&amp"
+
+$url = $out[0].Split("/url?q=")
+Start-Process
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PwshWebScraper
 PwshWebScraper is a scraper for all different categories (job searching, price comparisons, top links,  DevOps news)
+
+# prerequisites
+1. PowerShell Core
+2. Linux, OS X, or Windows
